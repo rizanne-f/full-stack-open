5451c0d1bc05865e71ae6cb6792ed87e1bf8502e
Wraith <70045536+rizanne-f@users.noreply.github.com>
2026-07-07 15:59:49 +0800

    Implement phonebook backend info page with response.send method

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -28,6 +28,13 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+    response.send(`
+        <div>Phonebook has info for ${persons.length} people</div>
+        <div>${new Date()}</div>
+    `)
+})
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on Port ${PORT}`)
